2b2f79fb3d63ba97f0aa63ff6fe6c71215d0b310
MaAb <marcel.ablasser@student.tugraz.at>
2018-11-15 09:08:51 -0800

    Created makefile for four verification tools.

--- C++/SimpleNullPointerBug/analyze.sh
+++ C++/SimpleNullPointerBug/analyze.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/opt/sonarqube-6.7.5/extensions/plugins/SourceMeter-8.2.0-x64-linux/CPP/SourceMeterCPP -projectName=simpleNullPointer -buildScript=build.sh -resultsDir=Results -externalSoftFilter=softfilter
+/opt/SourceMeter-8.2.0-x64-linux/CPP/SourceMeterCPP -projectName=simpleNullPointer -buildScript=build.sh -resultsDir=Results -externalSoftFilter=softfilter
--- C++/SimpleNullPointerBug/build.sh
+++ C++/SimpleNullPointerBug/build.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-#cd src
-
 make clean
-
-#make
 
 export SM_DISABLE_ANALYSIS=true
 ./configure
 unset SM_DISABLE_ANALYSIS
 
-#make
 make
+
