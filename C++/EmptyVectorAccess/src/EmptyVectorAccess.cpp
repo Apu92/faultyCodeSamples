f5fd863433b2f2922f279e36d4b86c939244a41a
MaAb <marcel.ablasser@student.tugraz.at>
2018-11-15 14:45:15 -0800

    Adapted EmptyVectorAccess.cpp

--- C++/EmptyVectorAccess/src/EmptyVectorAccess.cpp
+++ C++/EmptyVectorAccess/src/EmptyVectorAccess.cpp
@@ -1,10 +1,6 @@
 #include <vector>
 
-int foo(){
-  const std::vector<int> vec;
-  return vec[0]; // Empty vector access reported here
-}
-
 int main() {
-  foo();
+  const std::vector<int> vec;
+  return vec[0];
 }
