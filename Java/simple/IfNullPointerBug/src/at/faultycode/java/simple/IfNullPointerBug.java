db6f4f1c84d21eae22e7f163c44264fe87239f69
MaAb <marcel.ablasser@student.tugraz.at>
2018-11-17 11:15:48 -0800

    Remake SimpleNullPointerBug and created IfNullPointerBug project

---
+++ Java/simple/IfNullPointerBug/src/at/faultycode/java/simple/IfNullPointerBug.java
@@ -0,0 +1,21 @@
+package at.faultycode.java.simple;
+
+import java.util.logging.Logger;
+
+public class IfNullPointerBug {
+
+	private static final Logger LOGGER = Logger.getLogger(IfNullPointerBug.class.getName());
+	
+	public static void main(String[] args) {
+		
+		String message = null;
+		if (args.length == 2){
+			message = message.concat("We have a problem here.");
+		} else {
+			message = "No bug occurred! Try another number of arguments.";
+		}
+		
+		LOGGER.info(message);
+	}
+
+}
--- Java/simple/SimpleNullPointerBug/src/at/faultycode/java/simple/SimpleNullPointerBug.java
+++ Java/simple/SimpleNullPointerBug/src/at/faultycode/java/simple/SimpleNullPointerBug.java
@@ -9,13 +9,7 @@ public class SimpleNullPointerBug {
 	public static void main(String[] args) {
 		
 		String message = null;
-		if (args.length == 2){
-			message = message.concat("We have a problem here.");
-		} else {
-			message = "No bug occurred! Try another number of arguments.";
-		}
-		
-		LOGGER.info(message);
+		LOGGER.info(message.toString());
 	}
 
 }
